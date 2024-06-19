95c7cf7deecdbc571d489db22252b89df3f63dbe
SingHuyLeang <leang6525@gmail.com>
2024-06-19 12:06:19 +0700

    Loader

---
+++ lib/common/popuns/loader.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:note_app/common/style/colors.dart';
+
+class Loader {
+  static successSnackBar({ title = 'Note App', required message, duration = 3}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: NColors.light,
+      backgroundColor: NColors.primary,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: Duration(seconds: duration),
+      margin: const EdgeInsets.all(10),
+      icon: const Icon(Icons.check, color: NColors.light),
+    );
+  }
+
+  static warningSnackBar({title = 'Note App',required message}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: NColors.light,
+      backgroundColor: Colors.orange,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Icons.warning_rounded, color: NColors.light),
+    );
+  }
+
+  static errorSnackBar({title = 'Note App',required message}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: NColors.light,
+      backgroundColor: Colors.pink[900],
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Icons.error_sharp, color: NColors.light),
+    );
+  }
+}
--- lib/feature/app/controller/note_controller.dart
+++ lib/feature/app/controller/note_controller.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:note_app/common/popuns/loader.dart';
 import 'package:note_app/database/note_db.dart';
 import 'package:note_app/feature/app/model/note_model.dart';
 
@@ -25,14 +26,20 @@ class NoteController extends GetxController {
   void addNote() {
     try {
       if (titleController.text.isNotEmpty && noteController.text.isNotEmpty) {
-        NoteDatabase().insert(NoteModel(
-            id: 0,
-            title: titleController.text,
-            note: noteController.text,
-            date: currentDate()));
-      } else {}
+        NoteDatabase()
+            .insert(NoteModel(
+                id: 0,
+                title: titleController.text,
+                note: noteController.text,
+                date: currentDate()))
+            .whenComplete(
+                () => Loader.successSnackBar(message: "Note is added"));
+      } else {
+        Loader.warningSnackBar(message: "Please enter all field");
+      }
     } catch (e) {
-      log('message error ${e.toString()}');
+      Loader.errorSnackBar(message: "Something want wrong");
+      log("Error ${e.toString()}");
     } finally {
       titleController.text = "";
       noteController.text = "";
